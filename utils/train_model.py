5ff4cd444e39c09de2744fbcbe024e9617642d6a
Confidence Ledwaba <60347718+Confy255@users.noreply.github.com>
2020-05-26 11:52:29 +0200

    edited the file

--- utils/train_model.py
+++ utils/train_model.py
@@ -12,23 +12,23 @@
 # Dependencies
 import pandas as pd
 import pickle
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
 
 # Fetch training data and preprocess for modeling
-train = pd.read_csv('data/train_data.csv')
-riders = pd.read_csv('data/riders.csv')
-train = train.merge(riders, how='left', on='Rider Id')
+train_raw = pd.read_csv('Train.csv') 
+riders_raw = pd.read_csv('Riders.csv')
+train = train_raw.merge(riders_raw, how="left", on = "Rider Id")
 
-y_train = train[['Time from Pickup to Arrival']]
-X_train = train[['Pickup Lat','Pickup Long',
+ys_train = train[['Time from Pickup to Arrival']]
+Xs_train = train[['Pickup Lat','Pickup Long',
                  'Destination Lat','Destination Long']]
 
 # Fit model
-lm_regression = LinearRegression(normalize=True)
+lasso = Lasso(alpha=0.01)
 print ("Training Model...")
-lm_regression.fit(X_train, y_train)
+lasso.fit(Xs_train, ys_train)
 
 # Pickle model for use within our API
-save_path = '../trained-models/sendy_simple_lm_regression.pkl'
+save_path = '../trained-models/RForrest_model.pkl'
 print (f"Training completed. Saving model to: {save_path}")
-pickle.dump(lm_regression, open(save_path,'wb'))
+pickle.dump(lasso, open(save_path,'wb'))
