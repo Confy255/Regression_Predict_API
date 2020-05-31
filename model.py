bdaa79eb59836bcf978bd8f1d1835d42f9c0e4fa
Confidence Ledwaba <60347718+Confy255@users.noreply.github.com>
2020-05-31 16:13:49 +0200

    path_to_model

--- model.py
+++ model.py
@@ -114,7 +114,8 @@ def load_model(path_to_model:str):
         The pretrained model loaded into memory.
 
     """
-    return pickle.load(open(path_to_model='assets/trained-models/rfmodel_2.pkl', 'rb'))
+    path_to_model='assets/trained-models/rfmodel_2.pkl'
+    return pickle.load(open(path_to_model, 'rb'))
 
 def make_prediction(data, model):
     """Prepare request data for model prediciton.
